23e24aee06bb42d8a0d7dedff0227c7661f54c94
Thomas <thomas@Thomass-MacBook-Pro.local>
2022-08-03 18:10:02 -0700

    Test assert

--- tests/test_room.cpp
+++ tests/test_room.cpp
@@ -1,6 +1,16 @@
-
+/**
+ * @file test_room.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-08-03
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 
 #include <iostream>
+#include <assert.h>
 
 #include <room.h>
 
